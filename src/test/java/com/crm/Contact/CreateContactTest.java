d75a0d6126e0537684656f7a207222b6bb19fb5b
Abhi <abhisheksonu174@gmail.com>
2022-12-21 20:33:22 +0530

    First commit

---
+++ src/test/java/com/crm/Contact/CreateContactTest.java
@@ -0,0 +1,12 @@
+package com.crm.Contact;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+	@Test()
+	public void createContactsWithOrganizationNameTest(String browser)  {
+		System.out.println("a");
+	}
+}
+
+
